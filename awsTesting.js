3ab28d04603817bf4d5bdeef981eb98a04437c64
uptownnickbrown <uptownnickbrown@users.noreply.github.com>
2016-01-18 23:37:26 -0500

    Add some AWS testing
    To drop files in S3 buckets

---
+++ awsTesting.js
@@ -0,0 +1,15 @@
+var AWS = require('aws-sdk'),
+    awsCreds = require('./aws.json');
+
+AWS.config.update(awsCreds);
+
+var s3 = new AWS.S3();
+s3.listBuckets(function(err, data) {
+  if (err) { console.log("Error:", err); }
+  else {
+    for (var index in data.Buckets) {
+      var bucket = data.Buckets[index];
+      console.log("Bucket: ", bucket.Name, ' : ', bucket.CreationDate);
+    }
+  }
+});
