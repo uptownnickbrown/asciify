abc8f68a5e501b10e704ddae0cb3a14a9796884d
uptownnickbrown <uptownnickbrown@users.noreply.github.com>
2016-01-18 20:35:17 -0500

    Gut the express template down to barebones

--- routes/asciify.js
+++ routes/asciify.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* Got a request to asciify something */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
